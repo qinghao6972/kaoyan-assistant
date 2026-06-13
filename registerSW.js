a06753b29e0052ff1d8fb83b4e0a0108ee78cac1
qinghao <15659885+yu3575901878@user.noreply.gitee.com>
2026-06-13 21:18:25 +0800

    fix: correct base path to /kaoyan-assistant/

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/kaoyan-assistant/sw.js', { scope: '/kaoyan-assistant/' })})}
